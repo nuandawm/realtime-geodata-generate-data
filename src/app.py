d48cb676f9165b1c1291e24495e3825b48324f5a
Giuliano De Rossi <derossi.giuliano@gmail.com>
2022-08-15 00:09:23 +0000

    Add all necessary libraries

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
-import numpy as np
 import pandas as pd
+import contextily as cx
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import osmnx
+
+from keplergl import KeplerGl
+from shapely.geometry import shape
 
 if __name__ == "__main__":
-    series = pd.Series([1, 3, 5, np.nan, 6, 8])
-    print(series)
+    df_commuter = pd.read_csv("./data/data.csv")
+    df_commuter.head()
